8b52b9b94e97e4914b756ea3858344d95962d6d5
Arti <artisoni9201010@gmail.com>
2026-01-17 16:04:31 +0530

    add basics java file

---
+++ JavaBasicsFourth.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class JavaBasicsFourth {
+    public static void main(String[]  args){
+        // int counter = 0;
+        // while(counter<100){
+        //     System.out.println("hello world");
+        //     counter++;
+
+        // int counter = 1;
+        // while (counter <=10) {
+        //     System.out.print(counter + " ");
+        //     counter++;
+Scanner sc = new Scanner(System.in);
+        // int range = sc.nextInt();
+        // int counter = 1;
+        // while (counter<=range){
+        //     System.out.print(counter + " ");
+        //     counter++;
+
+       //
+        //System.out.println();
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // int counter = 1;
+        // while(counter<=n){
+        //     sum += counter;
+        //     counter++;
+        // }
+        // System.out.println("The sum is: " + sum);
+
+        // for( int i = 1; i <= 10 ;i++){
+        //     System.out.println(i + "hello world ");
+
+        // for(int line = 1 ;line <= 4;line++){
+        //     System.out.println("****");
+
+            // int line = 1;
+            // while(line <= 4){
+            //     System.out.println("****");
+            //     line++;
+
+            //print reverse of a number
+            // int n =10899;
+
+            // while(n > 0){
+            //     int lastDigit = n % 10;
+            //     System.out.println(lastDigit);
+            //     n = n/10;//n/=10
+            // }
+            // System.out.println();
+
+            //reverse the given number
+
+            // int n = 10899;
+            // int reverse = 0;
+            // while (n>0) {
+            //     int lastDigit = n % 10;
+            //     reverse = reverse * 10 + lastDigit;
+            //     n = n/10;
+            // }
+            // System.out.println(reverse);
+
+            // int counter = 1;
+            // do{
+            //     System.out.println("hello world");
+            //     counter++;
+            // }
+            // while(counter <= 10);
+
+            //break statement
+
+            
+
+            }
+        }
+    
+
+        
+    
+
