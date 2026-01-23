07ea408615d84c0cfc150a57c1670aafb2c99c2e
Arti <artisoni12.10.2003@gmail.com>
2026-01-23 16:11:57 +0530

    add swap functions

--- JavaFunctions.java
+++ JavaFunctions.java
@@ -1,7 +1,20 @@
+import java.util.*;
+
 public class JavaFunctions {
+
+    public static int sum(int num1, int num2) {
+        int sum = num1 + num2;
+     return sum;
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         
-        
-    }}
-        
-    
+        // int calculatesum = sum(a, b);
+        sum(a, b);
+         System.out.println("sum is :" + sum(a,b));
+
+    }
+}
---
+++ SwapFunctions.java
@@ -0,0 +1,17 @@
+public class SwapFunctions {
+
+public static void swap(int a , int b){
+int temp = a;
+          a = b ;
+          b = temp  ;
+         System.out.println(a);
+         System.out.println(b);
+
+    }
+
+    public static void main(String[] args) {
+        int a = 4;
+         int b = 5;
+swap(a, b);
+    }  
+}
